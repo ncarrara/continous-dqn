91e8317831032129ec110a8de33585f2c8fadcb7
Your Name <you@example.com>
2018-12-21 02:31:56 +0100

    some bug around generating traj and save, too much time

--- main/run.py
+++ main/run.py
@@ -3,32 +3,8 @@ import generate_samples
 import learn_autoencoders
 import test_and_base
 import configuration as c
-#
-# c.C.load("config/0.json")
-# generate_samples.main()
-# learn_autoencoders.main()
-# cross_comparaison.main()
-# test_and_base.main()
-# #
-# c.C.load("config/1.json")
-# generate_samples.main()
-# learn_autoencoders.main()
-# cross_comparaison.main()
-# test_and_base.main()
 
-# c.C.load("config/2.json")
-# generate_samples.main()
-# learn_autoencoders.main()
-# cross_comparaison.main()
-# test_and_base.main()
-
-# c.C.load("config/3.json")
-# generate_samples.main()
-# learn_autoencoders.main()
-# cross_comparaison.main()
-# test_and_base.main()
-
-c.C.load("config/4.json")
+c.C.load("config/0.json")
 generate_samples.main()
 learn_autoencoders.main()
 cross_comparaison.main()

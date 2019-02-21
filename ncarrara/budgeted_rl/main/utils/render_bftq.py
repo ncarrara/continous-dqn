dd5a852c13f11fca7ca307095b13d22a2d1c843d
Edouard Leurent <eleurent@gmail.com>
2019-02-21 19:34:17 +0100

    Add a render_bftq script
    Warning: introduce new dependency on eleurent:rl-agents 
    to generate videos with several frames per action

--- ncarrara/budgeted_rl/main/utils/render_bftq.py
+++ ncarrara/budgeted_rl/main/utils/render_bftq.py
@@ -2,25 +2,48 @@
 import os
 from multiprocessing.pool import Pool
 
-from ncarrara.budgeted_rl.tools.utils_run import execute_policy_from_config, format_results
+from PIL import Image
+
+from ncarrara.budgeted_rl.bftq.pytorch_budgeted_fittedq import PytorchBudgetedFittedQ, NetBFTQ
+from ncarrara.budgeted_rl.tools.features import feature_factory
+from ncarrara.budgeted_rl.tools.utils_run import execute_policy_from_config, format_results, get_action_mask
+from ncarrara.utils.datastructure import merge_two_dicts
 from ncarrara.utils.math_utils import set_seed, near_split, zip_with_singletons
 from ncarrara.utils.os import makedirs
-from ncarrara.budgeted_rl.tools.policies import PytorchBudgetedFittedPolicy
+from ncarrara.budgeted_rl.tools.policies import PytorchBudgetedFittedPolicy, policy_factory
 import numpy as np
 
 import logging
 
 from ncarrara.utils_rl.environments import envs_factory
-from ncarrara.utils_rl.environments.gridworld.envgridworld import EnvGridWorld
+from ncarrara.utils_rl.environments.envs_factory import get_actions_str
+from rl_agents.trainer.monitor import MonitorV2
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def main(betas_test, policy_path, generate_envs, feature_str, device, workspace, gamma, gamma_c,
-         bftq_params, seed, N_trajs, path_results, general, **args):
+def main(policy_path, generate_envs, feature_str, device, workspace, bftq_params, seed, general,
+         betas_test, N_trajs, gamma, gamma_c, bftq_net_params, **args):
     if not os.path.isabs(policy_path):
-        policy_path = workspace + "/" + policy_path
+        policy_path = workspace / policy_path
+
+    env = envs_factory.generate_envs(**generate_envs)[0][0]
+    feature = feature_factory(feature_str)
+
+    bftq = PytorchBudgetedFittedQ(
+        device=device,
+        workspace=workspace,
+        actions_str=get_actions_str(env),
+        policy_network=NetBFTQ(size_state=len(feature(env.reset(), env)), n_actions=env.action_space.n,
+                               **bftq_net_params),
+        gamma=gamma,
+        gamma_c=gamma_c,
+        cpu_processes=general["cpu"]["processes"],
+        env=env,
+        hull_options=general["hull_options"],
+        **bftq_params)
+    bftq.reset(True)
 
     pi_config = {
         "__class__": repr(PytorchBudgetedFittedPolicy),
@@ -28,50 +51,45 @@ def main(betas_test, policy_path, generate_envs, feature_str, device, workspace,
         "network_path": policy_path,
         "betas_for_discretisation": eval(bftq_params["betas_for_discretisation"]),
         "device": device,
-        "hull_options": general["hull_options"]
+        "hull_options": general["hull_options"],
+        "clamp_Qc": bftq_params["clamp_Qc"],
+        "env": env
     }
-    mock_env = envs_factory.generate_envs(**generate_envs)[0][0]
-    makedirs(workspace+"/"+"trajs")
+    pi = policy_factory(pi_config)
 
-    makedirs(path_results)
-    set_seed(seed)
-    try:
-        for beta in eval(betas_test):
-            # Prepare workers
-            cpu_processes = min(general["cpu"]["processes_when_linked_with_gpu"] or os.cpu_count(), N_trajs)
-            workers_n_trajectories = near_split(N_trajs, cpu_processes)
-            workers_seeds = np.random.randint(0, 10000, cpu_processes).tolist()
-            workers_params = list(zip_with_singletons(
-                generate_envs, pi_config, workers_seeds, gamma, gamma_c, workers_n_trajectories, beta,
-                None, "{}/beta={}.results".format(path_results, beta), general["dictConfig"]))
-            # Collect trajectories
-            # for params in workers_params:
-            #     print("------------------")
-            #     for param in params:
-            #         print(param)
-            logger.info("Collecting trajectories with {} workers...".format(cpu_processes))
-            with Pool(cpu_processes) as pool:
-                results = pool.starmap(execute_policy_from_config, workers_params)
-                rez = np.concatenate([result for _, result in results], axis=0)
+    # Iterate over betas
+    for beta in eval(betas_test):
+        logger.info("Rendering with beta={}".format(beta))
+        set_seed(seed, env)
+        for traj in range(N_trajs):
+            done = False
+            pi.reset()
+            info_env = {}
+            info_pi = {"beta": beta}
+            t = 0
 
-                trajs = []
-                for t, _ in results:
-                    trajs += t
-            print("BFTQ({:.2f}) : {}".format(beta, format_results(rez)))
+            # Make a workspace for trajectories
+            traj_workspace = workspace / "trajs" / "beta={}".format(beta) / "traj={}".format(traj)
+            makedirs(traj_workspace)
+            bftq.workspace = traj_workspace
+            monitor = MonitorV2(env, traj_workspace, add_subdirectory=False)
+            obs = monitor.reset()
 
-            if isinstance(mock_env,EnvGridWorld):
-                from ncarrara.utils_rl.environments.gridworld.world import World
-                w = World(mock_env)
-                w.draw_frame()
-                w.draw_lattice()
-                w.draw_cases()
-                w.draw_test_trajectories(trajs)
-                w.save(workspace+"/trajs/trajs_beta={:.2f}".format(beta))
-        if isinstance(mock_env, EnvGridWorld):
-            os.system("convert -delay 10 -loop 0 "+workspace+"/trajs/"+"*.png "+workspace+"/out.gif")
+            # Run trajectory
+            while not done:
+                action_mask = get_action_mask(env)
+                info_pi = merge_two_dicts(info_pi, info_env)
+                bftq.draw_Qr_and_Qc(obs, pi.network, "render_t={}".format(t), show=False)
+                a, _, info_pi = pi.execute(obs, action_mask, info_pi)
+                render(env, workspace, t, a)
+                obs, _, done, info_env = monitor.step(a)
+                t += 1
+            monitor.close()
 
-    except FileNotFoundError as e:
-        logger.warning("Could not load policy: {}".format(e))
+
+def render(env, workspace, time, action):
+    im = Image.fromarray(env.render(mode="rgb_array"))
+    im.save(workspace / "behavior" / "env_t={}_a={}.png".format(time, action))
 
 
 if __name__ == "__main__":
@@ -89,14 +107,13 @@ if __name__ == "__main__":
 
     from ncarrara.budgeted_rl.tools.configuration_bftq import C
 
-    C.load(config_file).load_pytorch().load_matplotlib('agg')
+    C.load(config_file).load_pytorch()
     if id:
-        C.workspace += "/{}".format(id)
+        C.workspace /= str(id)
         C.update_paths()
 
     main(device=C.device,
          seed=C.seed,
          workspace=C.path_bftq_egreedy,
-         path_results=C.path_bftq_results,
-         **C.dict["test_bftq"],
+         **C.dict["render_bftq"],
          **C.dict)

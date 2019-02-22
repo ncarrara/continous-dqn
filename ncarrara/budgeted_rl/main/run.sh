82b24bac206805ad30625ed4aa580aa82acdd300
Nicolas Carrara <nicolas.carrara1u@gmail.com>
2019-02-22 19:40:39 +0100

    w

--- ncarrara/budgeted_rl/main/run.sh
+++ ncarrara/budgeted_rl/main/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo $BASHPID
-#for i in $(seq $1 $2)
-for i in 0 1 1000 1001
+for i in $(seq $1 $2)
 do
      echo "Seed $i"
      python egreedy/main_egreedy.py config/slot_filling/camera_ready.json $i 1

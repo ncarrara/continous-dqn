e040ab7e1db2ccdd2773607b8cc8ff47fb99348f
Nicolas Carrara <nicolas.carrara1u@gmail.com>
2019-02-23 13:23:18 +0100

    bug fix (mauvais diviser pour l interval de confiance), ajout doption pour plot, marker

--- ncarrara/budgeted_rl/main/utils/plot_data.py
+++ ncarrara/budgeted_rl/main/utils/plot_data.py
@@ -7,6 +7,7 @@ import logging
 import re
 import pandas as pd
 import matplotlib
+
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 from matplotlib import patches
@@ -14,6 +15,7 @@ import seaborn as sns
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 
 def parse_data(path, params):
     # Extract results for each valid (algorithm, id) pair
@@ -60,40 +62,109 @@ def plot_all(data, path, params):
     std_of_means = means.groupby(['algorithm', 'parameter']).std().reset_index()
     mean_of_stds = stds.groupby(['algorithm', 'parameter']).mean().reset_index()
 
-    plot_patch(means_of_means, mean_of_stds, counts, x="Cd", y="Rd", curves="algorithm", points="parameter",
-               params=params, filename=os.path.join(path, "results_disc_extra.png"))
-    plot_patch(means_of_means, std_of_means, counts, x="Cd", y="Rd", curves="algorithm", points="parameter",
-               params=params, filename=os.path.join(path, "results_disc_intra.png"))
-    plot_patch(means_of_means, mean_of_stds, counts, x="C", y="R", curves="algorithm", points="parameter",
-               params=params, filename=os.path.join(path, "results_extra.png"))
-    plot_patch(means_of_means, std_of_means, counts, x="C", y="R", curves="algorithm", points="parameter",
-               params=params, filename=os.path.join(path, "results_intra.png"))
+    for show_annotation in [True, False]:
+        for type_varation in ["std", "ci"]:
+            for type in ["intra_seed", "extra_seed"]:
+                show_std = type_varation == "std"
+                show_ci = type_varation == "ci"
+                means = means_of_means
+                if type == "intra_seed":
+                    stds = mean_of_stds
+                elif type == "extra_seed":
+                    stds = std_of_means
+
+                plot_patch(type, means, stds, counts, show_annotation=show_annotation,
+                           show_std=show_std, show_confidence=show_ci, x="Cd", y="Rd",
+                           curves="algorithm", points="parameter",
+                           params=params,
+                           filename=os.path.join(path, "{}_{}_annot={}.png".format(type, type_varation,
+                                                                                   show_annotation)))
+
+            # plot_patch(means_of_means, mean_of_stds, counts, show_annotation=show_annotation,x="Cd", y="Rd", curves="algorithm", points="parameter",
+            #            params=params, filename=os.path.join(path, "results_disc_extra_annot={}.png".format(show_annotation)))
+            # plot_patch(means_of_means, std_of_means, counts,show_annotation=show_annotation, x="Cd", y="Rd", curves="algorithm", points="parameter",
+            #            params=params, filename=os.path.join(path, "results_disc_intra_annot={}.png".format(show_annotation)))
+            # plot_patch(means_of_means, mean_of_stds, counts, show_annotation=show_annotation,x="C", y="R", curves="algorithm", points="parameter",
+            #            params=params, filename=os.path.join(path, "results_extra_annot={}.png".format(show_annotation)))
+            # plot_patch(means_of_means, std_of_means, counts, show_annotation=show_annotation,x="C", y="R", curves="algorithm", points="parameter",
+            #            params=params, filename=os.path.join(path, "results_intra_annot={}.png".format(show_annotation)))
+
     try:
-        plot_lines(means, x='Cd', y='Rd', hue="algorithm", style="id", points="parameter", filename=os.path.join(path, "results_disc_ids.png"))
+        plot_lines(means, x='Cd', y='Rd', hue="algorithm", style="id", points="parameter",
+                   filename=os.path.join(path, "results_disc_ids.png"))
     except ValueError:
         logger.warning("Too many ids to use different styles")
-        plot_lines(means, x='Cd', y='Rd', hue="algorithm", points="parameter", filename=os.path.join(path, "results_disc_ids.png"))
+        plot_lines(means, x='Cd', y='Rd', hue="algorithm", points="parameter",
+                   filename=os.path.join(path, "results_disc_ids.png"))
 
 
-def plot_patch(mean, std, counts, x, y, curves, points, params, filename=None):
+def plot_patch(type, mean, std, counts, x, y, curves, points, params, show_std=False, show_annotation=True,
+               show_confidence=True, filename=None):
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     result = pd.concat([mean, std.add_suffix("_std"), counts.add_suffix("_count")], axis=1, sort=False)
+
+    # nb_run =
+
+    # handlers = []
+    labels = []
     for group_label, data in result.groupby(curves):
-        data.plot.scatter(x=x, y=y, ax=ax, c=[params[group_label][0]], zorder=2)
+        if type == "extra_seed":
+            nb_run = len(pd.unique(data["id_count"]))
+        elif type == "intra_seed":
+            nb_run = pd.unique(data["R_count"])
+            if len(nb_run) == 1:
+                nb_run = nb_run[0]
+            else:
+                raise Exception("malformed data")
+        else:
+            raise Exception("wrong type : {}".format(type))
+        print(type, nb_run)
+        # exit()
+        ax1 = data.plot.scatter(x=x, y=y, ax=ax, c=[params[group_label][0]], marker=params[group_label][1], s=30,
+                                zorder=2, label=group_label)
+        # ax1.legend(group_label)
+        # lines,_ = ax1.get_legend_handles_labels()
+        # handlers.append(lines)
+        # print(lines)
+        labels.append(group_label)
         for param, point in data.groupby(points):
-            sqr_n = np.sqrt(point[x + "_count"].values)
-            confidence_x = 1.96 * (point[x + "_std"].values / sqr_n)
-            confidence_y = 1.96 * (point[y + "_std"].values / sqr_n)
-            rect = patches.Rectangle((point[x].values - confidence_x, point[y].values - confidence_y),
-                                     2 * confidence_x, 2 * confidence_y,
-                                     linewidth=1.0,
-                                     fill=True,
-                                     edgecolor=(*params[group_label][0], 1),
-                                     facecolor=(*params[group_label][0], 0.2), zorder=0)
-            ax.add_patch(rect)
-            plt.annotate("{:.2f}".format(float(param)), (point[x].values, point[y].values))
-    plt.legend(handles=[patches.Patch(label=param[1], color=param[0]) for _, param in params.items()])
+            # print(point)
+            # sqr_n = np.sqrt(point[x + "_count"].values)
+            sqr_n = np.sqrt(nb_run)
+            # print("!!!!",point[x + "_count"].values)
+            if show_confidence:
+                confidence_x = 1.96 * (point[x + "_std"].values / sqr_n)
+                confidence_y = 1.96 * (point[y + "_std"].values / sqr_n)
+                rect = patches.Rectangle((point[x].values - confidence_x, point[y].values - confidence_y),
+                                         2 * confidence_x, 2 * confidence_y,
+                                         linewidth=1.0,
+                                         fill=True,
+                                         edgecolor=(*params[group_label][0], 1),
+                                         facecolor=(*params[group_label][0], 0.2), zorder=0)
+                ax.add_patch(rect)
+            if show_std:
+                std_x = point[x + "_std"].values
+                std_y = point[y + "_std"].values
+                rect = patches.Rectangle((point[x].values - std_x, point[y].values - std_y),
+                                         2 * std_x, 2 * std_y,
+                                         linewidth=1.0,
+                                         fill=True,
+                                         edgecolor=(*params[group_label][0], 1),
+                                         facecolor=(*params[group_label][0], 0.2), zorder=0)
+                ax.add_patch(rect)
+            if show_annotation:
+                plt.annotate("{:.2f}".format(float(param)), (point[x].values, point[y].values))
+
+    # plt.legend(handles=[patches.Patch(label=param[2], color=param[0],hatch=param[1]) for _, param in params.items()])
+    # plt.legend(handlers,
+    #            labels,
+    #            scatterpoints=1,
+    #            loc='lower left',
+    #            ncol=3,
+    #            fontsize=8)
+    # plt.legends()
+
     if filename:
         plt.savefig(filename)
     plt.show()
@@ -115,11 +186,12 @@ def plot_lines(data, x=None, y=None, filename=None, points=None, **kwargs):
 
 def main(workspace):
     palette = itertools.cycle(sns.color_palette())
+    marker = itertools.cycle(('^', 'v', '*', '+', '*'))
     algos = {
-        "ftq_duplicate": [next(palette), r"ftq duplicate($\lambda$)"],
-        "ftq_egreedy": [next(palette), r"ftq egreedy($\lambda$)"],
-        "bftq_egreedy": [next(palette), r"bftq egreedy($\beta$)"],
-        "bftq_duplicate": [next(palette), r"bftq duplicate($\beta$)"],
+        "ftq_duplicate": [next(palette), next(marker), r"ftq duplicate($\lambda$)"],
+        "ftq_egreedy": [next(palette), next(marker), r"ftq egreedy($\lambda$)"],
+        "bftq_egreedy": [next(palette), next(marker), r"bftq egreedy($\beta$)"],
+        "bftq_duplicate": [next(palette), next(marker), r"bftq duplicate($\beta$)"],
     }
     data = parse_data(workspace, algos)
     print(data)
